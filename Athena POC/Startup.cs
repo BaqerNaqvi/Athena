b8497c7df0c42f098cf70ee4d70371dc078efbfe
Baqer Naqvi <link2naqvi@gmail.com>
2018-09-11 18:58:25 +0500

    Code added
    Code added

---
+++ Athena POC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Athena_POC.Startup))]
+namespace Athena_POC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
